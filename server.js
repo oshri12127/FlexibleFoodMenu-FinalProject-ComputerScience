11b24e050201944c8901d02fed65b87f726ef0d3
oshri12127 <oshri12127@gmail.com>
2021-10-24 01:08:51 +0300

    index

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ app.get('/restaurantPage', (req, res) => {
 app.get('/index', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
-
+app.get('', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 app.use('/', (req, res) => {
   res.status(404).redirect('/404');
 });
